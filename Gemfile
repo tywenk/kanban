e7e2b299b3a9a507db0fd76016672a14dbbb15cb
tywen <tywen.kelly@gmail.com>
2022-04-16 13:52:24 -0700

    latest stuff

--- Gemfile
+++ Gemfile
@@ -51,3 +51,6 @@ gem 'active_model_serializers',
     '~> 0.10.12',
     git: 'https://github.com/jpawlyn/active_model_serializers.git',
     branch: '0-10-stable'
+
+#allows persistence of ordering of tasks on backend
+gem 'lexorank'
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" component={<Home />} />
+        <Route path="/login" component={<Login />} />
+        <Route path="/signup" component={<Signup />} />
+        <Route path="/boards" component={<Boards />}>
+          <Route path=":id" component={<Board />}></Route>
+          <Route path="tasks/:id" component={<TaskEdit />}></Route>
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
-)
+);
